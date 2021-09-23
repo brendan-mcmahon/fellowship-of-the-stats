61ad35d6bcc664c70a14468c3db8080d96b5e577
Brendan McMahon <brendan.mcmahon@bloomerang.com>
2021-09-23 11:50:59 -0400

    started react project

---
+++ fellowship-of-the-stats/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.scss'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <h1>Fellowship of the Stats</h1>
+      <h2>Test</h2>
+    </div>
+  )
+}
+
+export default App
